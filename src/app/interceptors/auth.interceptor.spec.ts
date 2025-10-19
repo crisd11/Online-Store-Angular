3b9c6614b255224258141cf835ee98564ee4172b
CrisD <cristianrivero.it@gmail.com>
2025-10-19 00:05:43 -0300

    staticwebapp.webconfig.json added

--- src/app/interceptors/auth.interceptor.spec.ts
+++
@@ -1,7 +0,0 @@
-import { AuthInterceptor } from './auth.interceptorts';
-
-describe('AuthInterceptor', () => {
-  it('should create an instance', () => {
-    expect(new AuthInterceptor()).toBeTruthy();
-  });
-});
